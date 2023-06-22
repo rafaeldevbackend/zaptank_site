25b69fd07c8d09c8771e00a58c9e2a3b9c61242a
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-06-22 18:19:33 -0300

    Armazenar hash JWT em cookie e refatorar código javascript

--- save_session.php
+++ save_session.php
@@ -3,7 +3,16 @@
 if(isset($_COOKIE['csrf_token']) && $_POST['csrf_token'] == $_COOKIE['csrf_token']) {
 	
 	if(session_status() !== PHP_SESSION_ACTIVE) session_start(['cookie_lifetime' => 2592000, 'cookie_secure' => true, 'cookie_httponly' => true]);	
-	  
+	 
+	setcookie(
+		$name = 'jwt_hash', 
+		$value = $_POST['jwt_hash'], 
+		$expires = time() + (60*60*24*30), 
+		$path = '/', 
+		$domain = 'http://localhost', 
+		$secure = true, 
+		$httponly = true
+	);
 	$_SESSION['UserId'] = $_POST['user_id'];
 	$_SESSION['UserName'] = $_POST['email'];
 	$_SESSION['PassWord'] = $_POST['password'];
