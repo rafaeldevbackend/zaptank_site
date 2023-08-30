57fb6861f7e0d0e26d5f68b93ca8cf3d4b7a2bca
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-08-30 18:07:22 -0300

    Consumir api e renderizar dados na página ranking de tempo online

--- onlinerankingtime.php
+++ onlinerankingtime.php
@@ -1,8 +1,6 @@
 <?php
 include 'globalconn.php';
-include 'getconnect.php';
 
-$Connect = Connect::getConnection();
 $_SESSION['Status'] = "Conectado";
 
 if (session_status() !== PHP_SESSION_ACTIVE)
@@ -23,46 +21,6 @@ if (empty($UserName) || $UserName == 0)
     header("Location: /");
     exit();
 }
-
-if (!empty($_GET['suv']))
-{
-    $i = $_GET['suv'];
-    $DecryptServer = $Ddtank->DecryptText($KeyPublicCrypt, $KeyPrivateCrypt, $i);
-    $query = $Connect->query("SELECT * FROM Db_Center.dbo.Server_List WHERE ID = '$DecryptServer'");
-    $result = $query->fetchAll();
-    foreach ($result as $infoBase)
-    {
-        $ID = $infoBase['ID'];
-        $BaseUser = $infoBase['BaseUser'];
-		$BaseTank = $infoBase['BaseTank'];
-    }
-}
-else
-{
-    header("Location: selectserver");
-    $_SESSION['alert_newaccount'] = "<div class='alert alert-danger ocult-time'>Não foi possível encontrar o servidor.</div>";
-    exit();
-}
-
-if (empty($ID) || empty($BaseUser))
-{
-    header("Location: selectserver");
-    exit();
-}
-
-$query = $Connect->query("SELECT COUNT(*) AS UserName FROM $BaseUser.dbo.Sys_Users_Detail where UserName = '$UserName'");
-$result = $query->fetchAll();
-foreach ($result as $infoBase)
-{
-    $CountUser = $infoBase['UserName'];
-}
-
-if ($CountUser == 0)
-{
-    header("Location: /selectserver?nvic=new&sid=$i");
-    exit();
-}
-   
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -89,88 +47,7 @@ if ($CountUser == 0)
                                           <th scope="col">Minutos Online</th>
                                        </tr>
                                     </thead>
-                                    <tbody>
-                                       <tr>
-                                          <th scope="row">1</th>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 228, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 238, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 248, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 258, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 268, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                       </tr>
-                                       <tr>
-                                          <th scope="row">2</th>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 229, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 239, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 249, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 259, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 269, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                       </tr>
-                                       <tr>
-                                          <th scope="row">3</th>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 230, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 240, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 250, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 260, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 270, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                       </tr>
-                                       <tr>
-                                          <th scope="row">4</th>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 231, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 241, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 251, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 261, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 271, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                       </tr>
-                                       <tr>
-                                          <th scope="row">5</th>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 232, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 242, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 252, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 262, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 272, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                       </tr>
-                                       <tr>
-                                          <th scope="row">6</th>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 233, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 243, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 253, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 263, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 273, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                       </tr>
-                                       <tr>
-                                          <th scope="row">7</th>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 234, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 244, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 254, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 264, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 274, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                       </tr>
-                                       <tr>
-                                          <th scope="row">8</th>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 235, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 245, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 255, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 265, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 275, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                       </tr>
-                                       <tr>
-                                          <th scope="row">9</th>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 236, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 246, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 256, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 266, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 276, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                       </tr>
-                                       <tr>
-                                          <th scope="row">10</th>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 237, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 247, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 257, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 267, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                          <td><?php $Ddtank->Rank($Connect, $Request = 277, ''.$BaseTank.'', ''.$BaseUser.''); ?></td>
-                                       </tr>
-                                    </tbody>
+                                    <tbody id="rank_list"></tbody>
                                  </table>
                               </div>
                            </div>
@@ -180,5 +57,71 @@ if ($CountUser == 0)
             </div>
          </div>
       </div>
+	  <script type="text/javascript" src="./js/utils/cookie.js"></script>
+	  <script type="text/javascript" src="./js/config.js"></script>
+	  <script type="text/javascript" src="./js/utils/url.js"></script>
+	  <script type="text/javascript" src="./js/functions.js"></script>
+	  <script type="text/javascript">
+
+         var error_div = document.getElementById('error');
+
+         var usp = new URLSearchParamsPolyfill(window.location.search);
+            
+         var suv = usp.get('suv');	
+
+         if(suv == null || suv == '') {
+            window.location.href = 'selectserver';
+         }
+
+         checkServerSuv(suv);
+         checkCharacter(suv);		
+
+         var url = `${api_url}/rank/online/list/${suv}`;
+         var jwt_hash = getCookie('jwt_authentication_hash');
+
+         var xhr = new XMLHttpRequest();
+
+         xhr.open('GET', url, true);
+         xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+         xhr.setRequestHeader('Content-type', 'application/json');
+         xhr.setRequestHeader('Authorization', `Bearer ${jwt_hash}`);
+		
+         xhr.onreadystatechange = function() {
+            if(xhr.readyState == 4) {
+               if(xhr.status == 200) {
+                  var response = JSON.parse(xhr.responseText);
+				  var data = response.data;
+				  
+                  var tbody = document.getElementById('rank_list');
+
+                  data.forEach(function(character, index) {
+					  
+					 var tr = document.createElement('tr');
+					  
+					 tr.innerHTML = `
+                        <th scope="row">${(index + 1)}</th>
+                        <td>${character.nickname}</td>
+                        <td>${character.level}</td>
+                        <td>${character.matches}</td>
+                        <td>${character.wins}</td>
+                        <td>${character.online_time}</td>
+                     `;
+					 
+					 tbody.appendChild(tr);
+                  });
+
+               } else if(xhr.status == 401) {
+                  error_div.innerHTML = `<div class='alert alert-danger ocult-time'>A sessão expirou, faça o login novamente.</div>`;
+                  setTimeout(function(){
+                     window.location.href = '/selectserver?logout=true';
+                  }, 1000);
+               } else {
+                  console.log("Erro na solicitação. Código do status: " + xhr.status);
+               }						
+            }
+         };
+         
+         xhr.send();
+	  </script>
    </body>
 </html>
