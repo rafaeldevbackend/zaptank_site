cbeb220f90ee8805b15d0417bf765808929ee84f
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-10-09 15:35:46 -0300

    Fix erro de instância.

--- Objects/gerenciamento.php
+++ Objects/gerenciamento.php
@@ -3,7 +3,6 @@ $Dados = new Conectado();
 $Ddtank = new Ddtank();
 $Modify = new Modify();
 $Pacotes = new Pacotes();
-$Loading = new Loading();
 $ServerList = new ServerList();
 $Conta = new Conta();
 ?>
