14692301965a377b69bcceeb6a975dee84b74820
Rafael Rodrigues Alves Teixeira <rafaelrat2019@gmail.com>
2023-11-10 16:15:39 -0300

    add: consulta status de fatura em segundo plano

--- Internal/fatura.php
+++ Internal/fatura.php
@@ -292,6 +292,42 @@ button:disabled {
 		xhr.send(params);
 	};
 	
+	setInterval(function(){
+		
+		var url = `${api_url}/invoice/status/${suv}?invoice_id=${invoice_token}`;
+		var jwt_hash = getCookie('jwt_authentication_hash');
+		
+		var xhr = new XMLHttpRequest();
+		
+		xhr.open('GET', url, true);
+		xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		xhr.setRequestHeader('Content-type', 'application/json');
+		xhr.setRequestHeader('Authorization', `Bearer ${jwt_hash}`);
+		
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState == 4) {
+				if(xhr.status == 200) {
+					var response = JSON.parse(xhr.responseText);
+					if(response.data.invoice.status == 'Aprovada') {
+						window.location.href = `/serverlist?suv=${suv}&page=success`;
+					}
+				} else if(xhr.status == 401) {
+					displayMessage(type = 'error', message = 'A sessão expirou, faça o login novamente.');
+					setTimeout(function(){
+						window.location.href = '/selectserver?logout=true';
+					}, 1000);
+				} else {
+					displayMessage(type = 'error', message = 'Houve um erro interno, se o problema persistir contate o administrador.');
+					setTimeout(function(){
+						window.location.href = '/';							
+					}, 2000);
+				}						
+			}
+		};
+		
+		xhr.send();
+	}, 5000);
+	
 	document.getElementById('pay_openpix').addEventListener('click', generateQrcodeOpenpix);
 	document.getElementById('pay_picpay').addEventListener('click', generateQrcodePicpay);
 </script>
